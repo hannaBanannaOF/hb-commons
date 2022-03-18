77275cbf8f01a304df1aa29383ee01b781c89c5c
Lucas Westphal <westphallucas1@outlook.com>
2022-03-18 18:17:29 -0300

    AJuste package name

--- hbcommons/apps.py
+++ hbcommons/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class HbcommonsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hbauth.hbcommons'
+    name = 'hbcommons'
