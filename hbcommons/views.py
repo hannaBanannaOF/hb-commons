4a417e307c36dde85ab8d12b37f8fc2ff5c29e92
Lucas Westphal <westphallucas1@outlook.com>
2022-04-15 17:03:13 -0300

    User serializers + view get-current-user

--- hbcommons/views.py
+++ hbcommons/views.py
@@ -1,6 +1,15 @@
 from .mixins import QueryStringParamSearchMixin
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.decorators import api_view
+from django.contrib.auth.decorators import login_required
+from rest_framework.response import Response
+
 # Create your views here.
+@api_view(['GET'])
+@login_required
+def current_user(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
 
 class QueryStringRetriveApiView(QueryStringParamSearchMixin, RetrieveAPIView):
     pass
