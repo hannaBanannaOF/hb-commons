18798ca6c67b239e00af6edbae01477422131220
Lucas Westphal <westphallucas1@outlook.com>
2022-04-15 17:55:24 -0300

    Ajuste

--- hbcommons/views.py
+++ hbcommons/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import RetrieveAPIView
 from rest_framework.decorators import api_view
 from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
+from .serializers import UserSerializer
 
 # Create your views here.
 @api_view(['GET'])
